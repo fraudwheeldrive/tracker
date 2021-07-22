ad463c5aeb306698919fdea3b19ef0ba0a1f38ce
[Andrew Core] <[adcore89@gmail.com]>
2021-07-21 20:35:15 -0400

    basic show model layout created:

---
+++ server/models/Show.js
@@ -0,0 +1,43 @@
+const { Schema } = require('mongoose');
+
+const showSchema = new Schema({
+
+    showName: [
+        {
+        type: String,
+    },
+    ],
+    description: {
+        type: String,
+        required: true,
+    },
+    // may need a show ID
+    
+    seasons: {
+        type: String,
+        require: true,
+    },
+    image: {
+        type:String,
+    },
+    time: {
+        type: String,
+        require: true,
+    },
+    streamingService: {
+        type: String,
+        require: true
+    },
+    });
+
+
+module.exports = Show; 
+
+
+// Show = showSchema 
+// showName = showName
+// seasons = seasons
+// description = descriptions
+// time = times 
+// whereIsItStreamed = streamingService
+// image = image 
