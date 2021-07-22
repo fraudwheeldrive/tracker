d48001dcc2db1cf6820f8201e3d9876a25a5e415
AimeeJay <ajesso@post.com>
2021-07-21 21:38:33 -0400

    user model is created and some authorization code added, currently, react app is still working

---
+++ server/models/User.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+const Order = require('./Show');
+// type User {  from typedefs
+//   _id: ID
+//   userName: String
+//   email: String
+//   password: String
+//   watchedShows: [Show]
+//   wishList: [Show]
+//   isPremium: Boolean
+// }  
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  watchedShows: [Show.schema],
+  wishList:[Show.schema],
+  isPremium: {
+      type: Boolean,
+      default: false
+  }
+});
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
+
