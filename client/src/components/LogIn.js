e9dbe6479bf83624e00fd8f8b4407c93c4d67b0e
[Andrew Core] <[adcore89@gmail.com]>
2021-07-22 19:58:06 -0400

    fixed changed to component/logins i accidentally wiped out

--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -1,48 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useMutation } from '@apollo/react-hooks';
+import Auth from "../utils/auth";
+import { ADD_USER } from "../utils/mutations";
 
+function Signup(props) {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [addUser] = useMutation(ADD_USER);
 
+  const handleFormSubmit = async event => {
+    event.preventDefault();
+    const mutationResponse = await addUser({
+      variables: {
+        email: formState.email, password: formState.password,
+        userName: formState.userName
+      }
+    });
+    const token = mutationResponse.data.addUser.token;
+    Auth.login(token);
+  };
 
-function LogIn(props) {
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  };
 
-//make sure links to are exact path
   return (
-    <form>
- <h2>Log In</h2>
-    <label>
-      <p>Username</p>
-      <input type="text" />
-    </label>
-    <label>
-      <p>Password</p>
-      <input type="password" />
-    </label>
-    <div>
-      <button type="submit">Submit</button>
+    <div className="box-center">
+      <h2>Signup</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="userName">Username:</label>
+          <input
+            placeholder="Username Here"
+            name="userName"
+            type="userName"
+            id="userName"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="email">Email:</label>
+          <input
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="******"
+            name="password"
+            type="password"
+            id="pwd"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row flex-end">
+          <button type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
-  </form>
   );
+
 }
 
-export default LogIn;
-
-
-// import React from 'react';
-
-// export default function Login() {
-//   return(
-//     <form>
-//       <label>
-//         <p>Username</p>
-//         <input type="text" />
-//       </label>
-//       <label>
-//         <p>Password</p>
-//         <input type="password" />
-//       </label>
-//       <div>
-//         <button type="submit">Submit</button>
-//       </div>
-//     </form>
-//   )
-// }
+export default Signup;
