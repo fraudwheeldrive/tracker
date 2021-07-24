6c0058ba2413923f6dbe92e653da0b2b136031f9
AimeeJay <ajesso@post.com>
2021-07-24 15:07:40 -0400

    fixing sign in page

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,83 +1,80 @@
-import React, { useState } from 'react';
+//signup backup
+
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { useMutation } from '@apollo/react-hooks';
-import { ADD_USER } from '../utils/mutations';
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
+import { ADD_USER } from "../utils/mutations";
 
-const Signup = () => {
-  
-  const [formState, setFormState] = useState({ username: '', email: '', password: '' });
-  const [addUser, { error }] = useMutation(ADD_USER);
+function Signup(props) {
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [addUser] = useMutation(ADD_USER);
 
-  // update state based on form input changes
+  const handleFormSubmit = async event => {
+    event.preventDefault();
+    const mutationResponse = await addUser({
+      variables: {
+        email: formState.email, password: formState.password,
+        userName: formState.userName
+      }
+    });
+    const token = mutationResponse.data.addUser.token;
+    Auth.login(token);
+  };
+
   const handleChange = event => {
     const { name, value } = event.target;
-
     setFormState({
       ...formState,
       [name]: value
     });
   };
 
-  // submit form
-  const handleFormSubmit = async event => {
-    event.preventDefault();
-
-    try {
-      const { data } = await addUser({
-        variables: { ...formState }
-      });
-
-      Auth.login(data.addUser.token);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-md-6">
-        <div className="card">
-          <h4 className="card-header">Sign Up</h4>
-          <div className="card-body">
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className="form-input"
-                placeholder="Your username"
-                name="username"
-                type="username"
-                id="username"
-                value={formState.username}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="Your email"
-                name="email"
-                type="email"
-                id="email"
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className="form-input"
-                placeholder="******"
-                name="password"
-                type="password"
-                id="password"
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button className="btn d-block w-100" type="submit">
-                Submit
-              </button>
-            </form>
 
-            {error && <div>Signup failed</div>}
-          </div>
+    <div className="box-center">
+      <h2>Signup</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="userName">Username:</label>
+          <input
+            placeholder="Username Here"
+            name="userName"
+            type="userName"
+            id="userName"
+            onChange={handleChange}
+          />
         </div>
-      </div>
-    </main>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="email">Email:</label>
+          <input
+            placeholder="youremail@test.com"
+            name="email"
+            type="email"
+            id="email"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row space-between my-2">
+          <label htmlFor="pwd">Password:</label>
+          <input
+            placeholder="******"
+            name="password"
+            type="password"
+            id="pwd"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex-row flex-end">
+          <button type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+
   );
-};
+
+}
 
 export default Signup;
