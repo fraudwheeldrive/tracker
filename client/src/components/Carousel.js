4417ebb984a3bac7fde033cd7542ae1c536de2c6
George Tsatalbasidis <adcore89@gmail.com>
2021-07-25 11:05:36 -0400

    inserted carousel code, added carousel data array, installed react-icons,

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,12 +1,45 @@
 import React from "react";
+import { SliderData } from './SliderData'
+import { FaArrowAltCircleRight,FaArrowAltCircleLeft } from 'react-icons/fa'
 import { Link } from "react-router-dom";
 
-function Carousel(props) {
 
-//make sure links to are exact path
-  return (
-    <h2>This is a Nav</h2>
-  );
-}
-
-export default Carousel;
+const Carousel = ({slides}) => {
+  const [current,setCurrent] = useState(0);
+  const length = slides.length;
+  
+  const nextSlide = () => {
+      setCurrent(current === length - 1 ? 0 : current + 1) 
+  };
+  
+  const prevSlide = () => {
+      setCurrent(current === 0 ? length -1 : current -1 )
+  };
+  
+  
+  if(!Array.isArray(slides) || slides.length <=0) {
+      return null;
+  }
+  
+      return (
+          <section className="slider">
+              <FaArrowAltCircleLeft className ="left-arrow" onClick={prevSlide} />
+              <FaArrowAltCircleRight className ="right-arrow" onClick={nextSlide} />
+          {SliderData.map((slide, index) => {
+              return ( 
+              
+              <div 
+              className={index === current ? 'slide active' : 'slide'} 
+              key={index}
+              > 
+              {index === current && (
+              <img src={slide.image} alt = "movie" className = 'image'/>
+              )} 
+              </div>
+              );
+          })}  
+      </section>
+      );  
+  };
+  
+  export default Carousel;
