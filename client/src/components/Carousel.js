362aa8917a521c5a3446593151e42e2d213dd38b
[Andrew Core] <[adcore89@gmail.com]>
2021-07-25 18:26:50 -0400

    add feature/carousel

--- client/src/components/Carousel.js
+++ client/src/components/Carousel.js
@@ -1,15 +1,4 @@
-// import React from "react";
 
-
-// function Carousel(props) {
-
-// //make sure links to are exact path
-//   return (
-//     <h2 className="box-center">Carousel area : Accept Andrew's incoming code and delete this</h2>
-//   );
-// }
-
-// export default Carousel;
 
 import React, {useState} from 'react'
 import { Link } from "react-router-dom";
--- client/src/components/CarouselData.js
+++ client/src/components/CarouselData.js
@@ -1,3 +1,28 @@
+
+// componentDidMount () {
+//   fetch("https://api.themoviedb.org/3/movie/popular?api_key=b50a54473311bbac83f4a96b6efa872d&language=en-US&page=1")
+//   .then(response => {
+//     console.log('response', response);
+//     if(!response.ok) {
+//       throw Error("Error fetching the movie image");
+//     }
+//     return response.json()
+//     .then(allData => {
+//       this.setState({ photos: allData });
+//     }) 
+//     .catch(err => {
+//       throw Error(err.message);
+//     });
+//   }
+//   );
+// }
+
+// // (image.results.poster_path)
+
+
+
+
+
 export const CarouselData = [
     {
       image:
