809944ae8ac3d1bc85e6e3ca44e7dcc22cdb86e3
[Andrew Core] <[adcore89@gmail.com]>
2021-07-27 21:29:04 -0400

    radial gradient added for background (body) FAQ updated with real words

--- client/src/components/GoPremium.js
+++ client/src/components/GoPremium.js
@@ -11,7 +11,7 @@ import Badge from 'react-bootstrap/Badge';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { FaRegCheckCircle,FaRegTimesCircle  } from "react-icons/fa";
+import { FaRegCheckCircle,FaRegTimesCircle, FaAndroid, FaApple } from "react-icons/fa";
 
 const checkStyle = { color: "green", fontSize: "2.3em" };
 const xStyle = { color: "red", fontSize: "2.3em" };
@@ -26,37 +26,33 @@ function GoPremium(props) {
   const accordionData = [
     {
       title: 'What Is PocketWatch?',
-      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-      laborum cupiditate possimus labore, hic temporibus velit dicta earum
-      suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-      voluptatem.`
+      content: `Pocket watch is the ultimate media tracking application. The application contains a slick and user 
+      focused UI paired with a robust database of Movies, Tv Shows and many other forms of media. With this application 
+      you can see whats on your favorite streaming apps, whats in the theater and even on the TV!. You will be able to 
+      discover new content that is geared to you, create and share your favorites, and build your ultimate sleepy sunday
+      movie list! `
     },
     {
       title: 'How much does Pocket Watch Cost? ',
-      content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
-      reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
-      quaerat iure quos dolorum accusantium ducimus in illum vero commodi
-      pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
-      quidem maiores doloremque est numquam praesentium eos voluptatem amet!
-      Repudiandae, mollitia id reprehenderit a ab odit!`
+      content: `Pocketwatch comes in 2 distinct flavours! we have a very functional free application, and by free we mean 
+      in cost and from advertisements. You will not need to answer surveys or sit through advertisements to unlock this apps
+      base functionality. Our Paid app unlocks Pocketwatch's full potential, and all for the low monthly cost of $3.99!. Please
+      refer to the chart above for the specifics of each model`
     },
     {
       title: 'What platforms are supported?',
-      content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
-      quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
-      dolor ut sequi minus iste? Quas?`
+      content: `Pocketwatch can be used in any browser and can be downloaded and used on your Android  and IOS devices `
     },
     {
       title: 'How Do I Cancel?',
-      content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
-      quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
-      dolor ut sequi minus iste? Quas?`
+      content: `You wont want too! but if you should need to we have a skilled team of customer care professionals who will 
+      assist you with this process`
     },
     {
       title: 'What Streaming Services can PocketWatch track?',
-      content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
-      quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
-      dolor ut sequi minus iste? Quas?`
+      content: ` Remember when it was just netflix? now we've come full circle and there almost as many apps as channels. Good news! 
+      Pocketwatch supports almost all of the them! We support a robust database containing shows from Netflix, Amazon prime, Hulu, Disney+, 
+      hollywood blockbusters, cable hits, syndicated legends and foreign films!`
     }
   ];
 
