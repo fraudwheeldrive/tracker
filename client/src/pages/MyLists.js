d60ed29758e234efe74ae747332956d65e02c466
AimeeJay <ajesso@post.com>
2021-07-31 13:51:06 -0400

    Created list pages accessible independently from searchbar page, conditionally renders if logged in

--- client/src/pages/MyLists.js
+++ client/src/pages/MyLists.js
@@ -4,28 +4,44 @@ import Container from "react-bootstrap/Container";
 import { MdAddShoppingCart } from "react-icons/md";
 import React, { useState, useEffect } from "react";
 import {
-  Jumbotron,
-  Col,
-  Row,
-  Form,
-  Button,
-  Card,
-  CardColumns,
+    Jumbotron,
+    Col,
+    Row,
+    Form,
+    Button,
+    Card,
+    CardColumns,
 } from "react-bootstrap";
 import Auth from "../utils/auth";
 import UserLoggedIn from '../pages/UserLoggedIn';
-const style = { color: "white", size:"50px" };
-const navStyle = {color: "black" };
+const style = { color: "white", size: "50px" };
+const navStyle = { color: "black" };
 
 function MyLists(props) {
+    //make sure links to are exact path
 
-  //make sure links to are exact path
-  return (
-    <div>
-        <h2>This is the lists page</h2>
-    </div>
-    
-  );
+    const isLoggedIn = Auth.loggedIn();
+    if (isLoggedIn)
+        return (<div className="box-center">
+            <h1>Logged in</h1>
+            <h2>Saved Lists</h2>
+        </div>
+        );
+
+    return (<div><h1>Please log in to see your lists</h1></div>);
+
+
+
+
+
+    //   return (
+    //     <div>
+    //         
+    //         {/* {Auth.loggedIn() && (<h6 className="right">Welcome back!</h6>)} */}
+
+    //     </div>
+
+    //   );
 }
 
 export default MyLists;
